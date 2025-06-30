b54bd5b18900a2059d015ffd638ed81c1dbfb408
been096 <been0562@gmail.com>
2025-06-30 12:12:20 +0900

    예복습

---
+++ Unity/Pratice/Pratice/Assets/Scripts/Dynamic.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class Dynamic : MonoBehaviour
+{
+    public float speed = 1;
+    public float JumpPower = 100;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += Vector3.right * speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position += Vector3.left * speed * Time.deltaTime;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+            GetComponent<Rigidbody2D>().AddForce(Vector3.up * JumpPower);
+    }
+}
