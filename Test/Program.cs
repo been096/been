ccb22d5279b6005989c03bae58bbd9a95a95bb23
been096 <been0562@gmail.com>
2025-07-01 15:55:32 +0900

    프로그래머스

---
+++ Test/Program.cs
@@ -0,0 +1,28 @@
+﻿using static Test.Program;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            solution(10);
+
+
+        }
+        public static int solution(int n)
+        {
+            int answer = 0;
+            n = 10;
+            for (int i = 0; i <= n; i++)
+            {
+                if (i % 2 == 0)
+                    answer += i;
+            }
+            Console.WriteLine(answer);
+
+            return answer;
+        }
+    }
+
+}
