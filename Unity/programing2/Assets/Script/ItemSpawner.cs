9de99e3059297a57186e7733d510579f84efa40d
Gyokujin <sotech3911@gmail.com>
2025-07-16 17:58:17 +0900

    0716 수업

---
+++ Unity/programing2/Assets/Script/ItemSpawner.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemSpawner : MonoBehaviour
+{
+    public Inventory inventory;
+    public ItemData testItem;
+    
+    
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.I) == true)
+        {
+            inventory.AddItem(testItem);
+            Debug.Log("아이템 추가됨");
+        }
+    }
+}
