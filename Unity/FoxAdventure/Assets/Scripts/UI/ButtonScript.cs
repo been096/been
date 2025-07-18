50a046d27e47b89922a426ff1727c793e6089d73
Gyokujin <sotech3911@gmail.com>
2025-07-18 17:53:51 +0900

    0718 수업

---
+++ Unity/FoxAdventure/Assets/Scripts/UI/ButtonScript.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.SceneManagement;
+using UnityEngine;
+
+public class Button : MonoBehaviour
+{
+    public GameObject cubeObject;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClick()
+    {
+        Instantiate(cubeObject, Vector3.zero, Quaternion.identity);
+    }
+}
